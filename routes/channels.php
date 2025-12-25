519645e9f0cb7c9d3d40251f5e0e3a59218c1670
Tasker Company <taskercompany7@gmail.com>
2025-12-25 19:08:10 +0500

    feat: Add broadcasting channels for users and conversations.

--- routes/channels.php
+++ routes/channels.php
@@ -14,19 +14,9 @@ Broadcast::channel('user.{id}', function ($user, $id) {
 
 // Conversation private channel
 Broadcast::channel('conversation.{conversationId}', function ($user, $conversationId) {
-    // User can access if they are assigned to the conversation or if they have permission
     $conversation = \App\Models\WhatsAppConversation::find($conversationId);
-    
     if (!$conversation) {
         return false;
     }
-    
-    // Allow if user is assigned to this conversation
-    if ($conversation->assigned_to === $user->id) {
-        return true;
-    }
-    
-    // Allow if user has permission to view all conversations
-    // You can add role/permission checks here
-    return true; // For now, allow all authenticated users
+    return true;
 });
